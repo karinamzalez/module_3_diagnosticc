3effc31f6ee33b6abe49c382172e46bef055e69c
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-12 09:23:46 -0600

    write feature test for user can log search alt fuel stations feature

---
+++ spec/features/user_can_search_alt_fuel_by_zip_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "user can search alternative stations" do
+  context "can search fuel stations by zip" do
+    scenario "user visits root & successfully searches fuel station" do
+# When I visit "/"
+    get "/"
+# And I fill in the search form with 80203
+    within "#search"
+      fill_in "zip", :with => 80203
+    end
+# And I click "Locate"
+    click_button "Locate"
+# Then I should be on page "/search" with parameters visible in the url
+    expect(page).to have_current_path(search_path(zip: 80203))
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    within "#close-locations"
+      expect(page).to 
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+    end
+  end
+end
