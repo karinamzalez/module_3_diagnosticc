e6b3946ee761ed8a22e8d61ef608860a48fea5b8
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-12 09:39:51 -0600

    fix spec

--- spec/features/user_can_search_alt_fuel_by_zip_spec.rb
+++ spec/features/user_can_search_alt_fuel_by_zip_spec.rb
@@ -5,15 +5,15 @@ RSpec.feature "user can search alternative stations" do
 # When I visit "/"
     get "/"
 # And I fill in the search form with 80203
-    within "#search"
-      fill_in "zip", :with => 80203
+    within "#search" do
+      fill_in "zip", :with => "80203"
     end
 # And I click "Locate"
     click_button "Locate"
 # Then I should be on page "/search" with parameters visible in the url
     expect(page).to have_current_path(search_path(zip: 80203))
 # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-    within "#close-locations"
+    within "#close-locations" do
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
     end
